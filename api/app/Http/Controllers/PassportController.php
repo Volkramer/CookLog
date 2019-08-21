fe455f2a47dbc451855395f2e49bb183f8063b6d
Frederic Fleisch <frederic.fleisch@epitech.eu>
2019-08-21 10:48:01 +0200

    fix: api register with passport route /oauth/token

--- api/app/Http/Controllers/PassportController.php
+++ api/app/Http/Controllers/PassportController.php
@@ -4,21 +4,24 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Laravel\Passport\Client;
+use GuzzleHttp\Client as HttpClient;
 
 class PassportController extends Controller
 {
-    /**
-     * Handles Registration Request
-     *
-     * @param Request $request
-     * @return \Illuminate\Http\JsonResponse
-     */
+    private $client;
+
+    public function __construct()
+    {
+        $this->client = Client::find(1);
+    }
+
     public function register(Request $request)
     {
         $this->validate($request, [
             'name' => 'required|min:3',
             'email' => 'required|email|unique:users',
-            'password' => 'required|min:6',
+            'password' => 'required|min:6|confirmed',
         ]);
 
         $user = User::create([
@@ -26,17 +29,23 @@ class PassportController extends Controller
             'email' => $request->email,
             'password' => bcrypt($request->password)
         ]);
-        $token = $user->createToken('CookLog')->accessToken;
 
-        return response()->json(['token' => $token], 200);
+        $http = new HttpClient();
+
+        $response = $http->post(env('APP_URL') . '/oauth/token', [
+            'form_params' => [
+                'grant_type' => 'password',
+                'client_id' => $this->client->id,
+                'client_secret' => $this->client->secret,
+                'username' => $user->email,
+                'password' => $request->password,
+                'scope' => '*',
+            ],
+        ]);
+
+        return json_decode((string) $response->getBody(), true);
     }
 
-    /**
-     * Handles Login Request
-     *
-     * @param Request $request
-     * @return \Illuminate\Http\JsonResponse
-     */
     public function login(Request $request)
     {
         $credentials = [
@@ -51,11 +60,6 @@ class PassportController extends Controller
         }
     }
 
-    /**
-     * Return Authenticated User Details
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
     public function details()
     {
         return response()->json(['user' => auth()->user()], 200);
