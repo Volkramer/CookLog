0b3e0d32c97f3fc9f85660b276665f0a35297266
Volkramer <volkramer@gmail.com>
2019-09-07 09:46:23 +0200

    add: shopping_list_content model/migration

---
+++ api/database/migrations/2019_09_07_053638_create_shopping_list_contents_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShoppingListContentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shopping_list_contents', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->int('quantity');
+            $table->bool('check')->default(0);
+            $table->bigInteger('shopping_list_id')->unsigned();
+            $table->bigInteger('product_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shopping_list_contents');
+    }
+}
