df92090d692a87c191639b3a05fec2413e629b07
Volkramer <volkramer@gmail.com>
2019-09-21 18:39:33 +0200

    add: database mongodb driver

--- api/app/product.php
+++ api/app/product.php
@@ -2,17 +2,9 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model;
 
 class Product extends Model
 {
-    public function shoppingListContents()
-    {
-        return $this->belongsToMany('App\ShoppingListContent');
-    }
-
-    public function stockContents()
-    {
-        return $this->belongsToMany('App\StockContent');
-    }
+    protected $connection = "mongodb";
 }
