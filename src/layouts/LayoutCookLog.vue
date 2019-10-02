359a6ca18e617baa74a98cb7dbed108d584d5815
Guile <g.captus@gmail.com>
2019-10-02 12:28:32 +0200

    gitflow-feature-stash: layout
    nettoyage d'App et de Home avec ajout d'un layout dans un seul fichier

---
+++ src/layouts/LayoutCookLog.vue
@@ -0,0 +1,103 @@
+<template>
+  <v-app id="app">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+        color="#ff880a"
+    >
+      <template v-slot:prepend >
+        <v-list-item two-line color="#1ea80f">
+          <v-list-item-avatar>
+            <img src="https://randomuser.me/api/portraits/lego/8.jpg">
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>Filippo Etchebesti</v-list-item-title>
+            <v-list-item-subtitle>Logged In</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+
+      <v-divider></v-divider>
+      <v-list>
+        <v-list-item @click="this.$router.push({ name: 'stock'})">
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="this.$router.push({ name: 'stock'})">
+          <v-list-item-action>
+            <!-- 
+              les icones se trouvent ici, penser à vérifier que les - remplacent les _
+              https://material.io/resources/icons/?style=baseline 
+              -->
+            <v-icon>mdi-folder</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Inventaire</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="router.push({ name: 'recettes'})">
+          <v-list-item-action>
+            <v-icon>mdi-format-list-bulleted</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Recettes</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="router.push({ name: 'shopping-list'})">
+          <v-list-item-action>
+            <v-icon>mdi-cart-arrow-down</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Listes de Courses</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="#1ea80f"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>CookLog</v-toolbar-title>
+      <div class="flex-grow-1"></div>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      
+    </v-content>
+    <v-footer
+      app
+      color="#1ea80f"
+      dark
+    >
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        &copy; {{ new Date().getFullYear() }} — <strong>Les 3 Grabataires</strong>
+      </v-col>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+    }),
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,21 @@
 <template>
-  <HelloWorld />
+  <LayoutCookLog>
+    <v-content>
+      <HelloWorld />
+    </v-content>
+      
+  </LayoutCookLog>
 </template>
 
 <script>
 import HelloWorld from '../components/HelloWorld';
+import LayoutCookLog from "../layouts/LayoutCookLog";
 
 export default {
   components: {
     HelloWorld,
+    LayoutCookLog,
   },
 };
 </script>
+
