b58775c4ec2a98eb6010186c4555b9c3f510e088
Guile <g.captus@gmail.com>
2019-10-10 06:21:25 +0200

    correction of a route

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import HomePage from './views/HomePage.vue'
 import ListStockListsPage from "@/views/ListStockListsPage";
 import StockListPage from "@/views/StockListPage";
 
@@ -13,7 +13,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: HomePage
     },
     {
       path: '/about',
@@ -24,7 +24,7 @@ export default new Router({
       component: () => import(/* webpackChunkName: "about" */ './views/AboutPage.vue')
     },
     {
-      path: '/stock',
+      path: '/stock/:id',
       name: 'stock',
       component: StockListPage
     },
