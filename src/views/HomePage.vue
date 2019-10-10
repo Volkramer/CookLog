d51449984f911d7e437de77ec21ca852c16dc1c1
Guile <g.captus@gmail.com>
2019-10-10 04:47:20 +0200

    update router + add page used by router
    ajout des page pour les listes de stocks, stock, ainsi que modification de la page home et du nom de la page about, plus mise à jour du router

---
+++ src/views/HomePage.vue
@@ -0,0 +1,32 @@
+<template>
+
+    <v-content>
+      <v-container>
+        <h1>Bienvenue sur CookLog</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt obcaecati officiis tenetur. Id a aliquid totam explicabo et earum atque sed, dolorum ducimus voluptatum, temporibus tenetur eveniet adipisci, ipsum fuga.</p>
+        <h3>Si vous voulez vous inscrire :</h3>
+        <v-btn icon>
+          <v-icon>mdi-account-plus</v-icon>
+        </v-btn>
+        <h3>Connectez-vous</h3>
+        <v-btn icon>
+          <v-icon>mdi-login</v-icon>
+        </v-btn>
+      </v-container>
+    </v-content>
+      
+
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  components: {
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
+
