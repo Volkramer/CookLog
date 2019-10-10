c3eee95857926f72a80c95ea1796cd4e5d83a658
Guile <g.captus@gmail.com>
2019-10-10 04:53:48 +0200

    update liststocklistspage.vue, add stocklistcard.vue and stockservice
    ajout de contenu dans la page d'affichage des listes de stock. création de la card utilisé dans la page mise à jour. ajout du fichier de config pour requête axios pour les stock

---
+++ src/components/StockListsCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <router-link class="event-link" :to="{ name: 'event-show', params: { id: stockList.id } }">
+    <v-card
+      max-width="480"
+      class="mx-auto"
+    >
+      
+      <v-list-item three-line>
+        
+        <v-icon size="40">mdi-folder</v-icon>
+        
+        <v-list-item-content>
+          <v-list-item-title class="headline mb-1">{{stockList.name}}</v-list-item-title>
+        </v-list-item-content>
+
+        <v-card-actions>
+          <v-icon>mdi-trash-can-outline</v-icon>
+          <v-icon>mdi-file-document-edit</v-icon>
+        </v-card-actions>
+      </v-list-item>      
+      
+    </v-card>
+  </router-link>
+</template>
+
+<script>
+    export default {
+      props:{
+        stockList: Object,
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
