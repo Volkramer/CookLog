65a72a392196defd67b564c62f85994b2a5ecaf0
Guile <g.captus@gmail.com>
2019-10-15 18:10:53 +0200

    add a signup page at /signup
    not functionnal, just the front

---
+++ src/views/SignUpPage.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-card max-width="600px">
+      <v-card-title>
+        <h2 class="title">Enregistrez vous</h2>
+      </v-card-title>
+      <v-card-text>
+        <v-form class="px-3">
+          <v-text-field label="username" v-model="username" required></v-text-field>
+          <v-text-field label="email" v-model="email" required></v-text-field>
+          <v-text-field label="password" v-model="password" required></v-text-field>
+        </v-form>
+      </v-card-text>
+    </v-card>
+
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        username:'',
+        email:'',
+        password:'',
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
