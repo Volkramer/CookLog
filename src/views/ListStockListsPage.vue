b9ed607ffde9d071f4fb43a4b60ddb0a57b5a136
Guile <g.captus@gmail.com>
2019-10-17 21:04:06 +0200

    add button in ListStockListsPage with form in modal to create stock
    juste le modal et le formulaire, pas de fonction dedans

--- src/views/ListStockListsPage.vue
+++ src/views/ListStockListsPage.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
     <h2>Liste des stocks</h2>
+    <br>
+    <AddStockToList />
+    <br>
     <StockListsCard v-for="stockList in stockLists" :key="stockList.id" :stockList="stockList" />
   </div>
 </template>
 
 <script>
-import StockListsCard from "../components/StockListsCard.vue";
+import StockListsCard from "@/components/StockListsCard.vue";
 import StockService from "../services/StockService";
+import AddStockToList from "@/components/AddStockToList";
 
   export default {
     components:{
       StockListsCard,
+      AddStockToList,
     },
     data(){
       return{
